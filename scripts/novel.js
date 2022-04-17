8a05e954315dd5aa00232873bbd81bf5adefe086
adrianmahk@qingsky <qingskys@gmail.com>
2022-04-17 21:45:46 +0800

    [js] fix adjust title error in home

--- scripts/novel.js
+++ scripts/novel.js
@@ -317,14 +317,16 @@ function replaceTitle() {
 }
 
 function adjustTitle() {
-    const title = document.getElementById("post-title") || document.getElementById("search-label");
-    const topBar = document.getElementById("top-bar");
-    // console.log(window.getComputedStyle(title).width, topBar.getBoundingClientRect().width - 80);
-    if (title.clientWidth >= topBar.clientWidth - 80) {
-        title.style.transform = document.body.classList.contains("collapsed-header") ? "scale(0.8)" : "";
-    }
-    else {
-        title.style.transform = '';
+    if (document.body.classList.contains("feed-view") || document.body.classList.contains("item-view")) {
+        const title = document.getElementById("post-title") || document.getElementById("search-label");
+        const topBar = document.getElementById("top-bar");
+        // console.log(window.getComputedStyle(title).width, topBar.getBoundingClientRect().width - 80);
+        if (title.clientWidth >= topBar.clientWidth - 80) {
+            title.style.transform = document.body.classList.contains("collapsed-header") ? "scale(0.8)" : "";
+        }
+        else {
+            title.style.transform = '';
+        }
     }
 }
 
