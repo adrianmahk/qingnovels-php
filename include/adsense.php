764aa8c777ed178f0831198e63cc735e088ede44
adrianmahk@qingsky <qingskys@gmail.com>
2022-05-23 14:35:26 +0800

    [adsense, css] try adding another ad for dark mode

--- include/adsense.php
+++ include/adsense.php
@@ -1,5 +1,11 @@
 <script async='async' src='https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js'/></script>
 <ins class='adsbygoogle' data-ad-client='ca-pub-6259217392436204' data-ad-format='fluid' data-ad-layout-key='-gw-3+1f-3d+2z' data-ad-slot='3824983146' style='display:block'></ins>
+<ins class="adsbygoogle dark"
+     style="display:block"
+     data-ad-format="fluid"
+     data-ad-layout-key="-gw-3+1f-3d+2z"
+     data-ad-client="ca-pub-6259217392436204"
+     data-ad-slot="5576397138"></ins>
 <script>
-    (adsbygoogle = window.adsbygoogle || []).push({});
+     (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
